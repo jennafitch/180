5dcd4f3c4c66dd9a6785103f75039bcb014366a1
jennafitch <fitchj25@uww.edu>
2024-04-03 10:21:04 -0500

    Commit with Yankees

---
+++ fib.py
@@ -0,0 +1,14 @@
+# This is a python program to calculate the Fibonacci sequence. Create the program to do this.
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+    
+n = int(input("Enter the number of terms: "))
+print("Fibonacci sequence:")
+for i in range(n):
+    print(fibonacci(i))
+
+print ("Go Yankees")
